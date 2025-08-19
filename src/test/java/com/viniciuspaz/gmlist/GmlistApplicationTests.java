b89e63003d8d0ae1576b5cb8216a60d93839a9b8
V1n1Paz <viniciuspaz@live.com>
2025-08-18 22:58:18 -0300

    project created

---
+++ src/test/java/com/viniciuspaz/gmlist/GmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuspaz.gmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
