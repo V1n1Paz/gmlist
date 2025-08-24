29f87fc8ee0c24dd14a3825cc8fc95b35a3ed40d
V1n1Paz <viniciuspaz@live.com>
2025-08-24 20:12:50 -0300

    homolog ok

--- src/main/java/com/viniciuspaz/gmlist/projections/GameMinProjection.java
+++ src/main/java/com/viniciuspaz/gmlist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.viniciuspaz.gmlist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
